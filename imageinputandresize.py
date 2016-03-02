fc7723d29542a92b723905875ac458b22c2e26db
Drearam3 <andramirez@csumb.edu>
2016-03-02 15:15:55 -0800

    Code to input images into array and resize them

---
+++ imageinputandresize.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+#putting all photos into an array
+
+filename = ".jpg"
+imArray = [""]
+
+#counter for photos
+    #Each image is a number from 1-57
+photoNumCount = 1
+for i in range (0, 56):
+    #Changes the int photoNumCount to a string
+    imNum2 = str(photoNumCount)
+    #Appends the string number to ".jpg" to create the full filename
+    filename2 = imNum2 + filename
+    imArray.append(Image.open(filename2))
+    photoNumCount +=1
+
+
+#calling random photos from array and compressing them to be 400x400
+num = imArray[15]
+##num.show()
+num = num.resize((160, 90), Image.ANTIALIAS)
+num.show()
